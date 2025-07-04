9f5169c3429fd5bc9a3fa939d87c1d78a709a9c1
Gabriel Castro <josegabrielsouzacastro@gmail.com>
2025-07-03 22:39:06 -0300

    header create

--- NFPro/src/components/ClientsPage/ClientsHeader.tsx
+++ NFPro/src/components/ClientsPage/ClientsHeader.tsx
@@ -1,29 +1,32 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { useTheme, Text } from "react-native-paper"
+import { useTheme, Text } from "react-native-paper";
 export default function HeaderClient() {
-      const { colors } = useTheme();
+  const { colors } = useTheme();
 
-    return (
-        <View style={[styles.header, {backgroundColor: colors.background}]}>
-            <Text variant="titleMedium" style={[styles.title, {color: colors.primary}]}>Clientes</Text>
-        </View>
-    );
+  return (
+    <View style={[styles.header, { backgroundColor: colors.background }]}>
+      <Text
+        variant="titleMedium"
+        style={[styles.title, { color: colors.primary }]}
+      >
+        Clientes
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: '12%',
-        width: '100%',
-        padding: 10,
-        justifyContent: 'center'
-
-        
-    },
-    title: {
-        fontWeight: 'bold',
-        alignContent: 'flex-start',
-        marginTop: 40,
-        left: 10,
-    },
+  header: {
+    height: "12%",
+    width: "100%",
+    padding: 10,
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    alignContent: "flex-start",
+    marginTop: 40,
+    left: 10,
+  },
 });
